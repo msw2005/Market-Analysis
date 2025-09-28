4875eab5cb331700d8a0df9941f933b6c20f65df
M Sadiq Warsi <157520386+msw2005@users.noreply.github.com>
2025-09-28 11:07:16 +0100

    Update EPS and P-E.py

--- EPS and P-E.py
+++ EPS and P-E.py
@@ -10,7 +10,7 @@ info = ticker.info
 The time period for these metrics is determined by Yahoo Finance, which provides the data. The "trailing" in "trailing P/E" and "trailing EPS" typically means
 these metrics are calculated using data from the past 12 months.
 '''
-# Get metrics
+# Get the metrics
 pe_ratio = info.get('trailingPE')
 eps = info.get('trailingEps')
 
